c58d4c322a53ce6ba27807f26c0124bd72b6c5b4
Fryhw <81508836+Fryhw@users.noreply.github.com>
2025-10-13 23:59:01 +0200

    'up'

--- chessy/utils.py
+++ chessy/utils.py
@@ -1,6 +1,14 @@
 import torch
 import numpy as np
 
+def start_game():
+    game = ''
+    return game
+
+def stop_game(game):
+    if game != None : 
+        return 'No game'
+    return 'Game stopped'
 def game_info(game):
     if game != None : 
         return 'No_game'
